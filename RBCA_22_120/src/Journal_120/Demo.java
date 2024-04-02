f8c9e205996209101d3e3b80c88a0ce528d60834
Ciya17 <ciyasilva1278@gmail.com>
2024-04-03 01:53:36 +0530

    commit n pull demo

--- RBCA_22_120/src/Journal_120/Demo.java
+++ RBCA_22_120/src/Journal_120/Demo.java
@@ -20,6 +20,7 @@ public class Demo {
 	        System.out.println("Difference: " + difference);
 	        System.out.println("Product: " + product);
 	        System.out.println("Quotient: " + quotient);
+	        System.out.println("Eclipse");
 
 	        scanner.close();
 	    }
